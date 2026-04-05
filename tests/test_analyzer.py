b1460c0bb4920329cd3b96b61c4da2f40b1cb1c8
niklasblock <niklas.block.business@gmail.com>
2026-04-05 18:59:55 +0200

    add unit tests for missing docstring check

---
+++ tests/test_analyzer.py
@@ -0,0 +1,70 @@
+#tests/test_analyzer.py 
+
+import ast 
+from pycheck.analyzer import Analyzer
+from pathlib import Path 
+import pytest
+import textwrap
+
+def test_missing_docstring(): 
+    """one function with Docstring
+        -> one warning
+    """
+
+    source = textwrap.dedent("""
+    def foo(): 
+        pass 
+    """)
+
+    tree = ast.parse(source) 
+    analyzer = Analyzer(Path("dummy.py"))
+
+    warnings = analyzer._check_missing_docstrings(tree) 
+
+    assert len(warnings) == 1 
+    assert "foo" in warnings[0]
+
+def test_no_missing_docstring(): 
+    """one function with Docstring 
+        -> no warning, empty list 
+    """
+
+    source = textwrap.dedent('''
+    def bar():
+        """Diese hat einen Docstring."""
+        pass
+    ''')
+
+    tree = ast.parse(source)
+    analyzer = Analyzer(Path("dummy.py"))
+
+    warnings = analyzer._check_missing_docstrings(tree)
+
+    assert len(warnings) == 0 
+    assert warnings == []
+
+def test_multiple_missing(): 
+    """three functions, two without Docstring
+        -> two warnings 
+    """
+
+    source = textwrap.dedent('''
+    def foo():
+        pass
+
+    def bar():
+        """Diese hat einen Docstring."""
+        pass
+
+    def baz():
+        pass
+    ''')
+
+    tree = ast.parse(source)
+    analyzer = Analyzer(Path("dummy.py"))
+
+    warnings = analyzer._check_missing_docstrings(tree) 
+
+    assert len(warnings) == 2
+    assert "foo" in warnings[0]
+    assert "baz" in warnings[1]
